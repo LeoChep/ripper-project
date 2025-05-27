9f01ce04d88d3565d143b7fdffe89b8ca55da6da
LeoChep <1515038222@qq.com>
2025-05-27 20:47:11 +0800

    增加游戏面板测试数据

--- src/components/GamePannel/GamePannel.vue
+++ src/components/GamePannel/GamePannel.vue
@@ -1,10 +1,37 @@
 <template>
-    <div class="game-pannel">
-       
+    <div class="game-pannel" id="game-pannel">
+
     </div>
 </template>
 
 <script setup>
+import testImg from '@/assets/logo.svg'
+import * as PIXI from 'pixi.js'
+import { onMounted } from 'vue'
+onMounted(async () => {
+    const app = new PIXI.Application();
+    await app.init({
+        width: 800,
+        height: 600,
+        antialias: true,
+        background: 0x262626, // 设置canvas背景颜色
+        backgroundAlpha: 0,   // 设置背景颜色透明度
+        resolution: 1,
+        // transparent: false, // backgroundAlpha 已控制透明度
+    });
+
+    document.getElementById("game-pannel").appendChild(app.canvas);
+
+    // TODO 路径暂时写死
+    await PIXI.Assets.load(testImg);
+    let bear = PIXI.Sprite.from(testImg);
+    bear.x = 400;
+    bear.y = 300;
+    app.stage.addChild(bear);
+    app.stage.interactive = true;
+})
+
+
 </script>
 
 <style scoped>
@@ -15,5 +42,5 @@
     width: 100vw;
     height: 100vh;
     background: white
-  }
+}
 </style>
